18a873cec0e9e0538db3c5835df3fc0be3bf82f8
Mike Rivera <rivera.mj@gmail.com>
2014-10-12 23:04:12 -0400

    move JS to app.js

--- html/js/app.js
+++ html/js/app.js
@@ -1,3 +1,17 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).ready(function() {
+  $('#samples').smoothScroll();
+
+  $("section.sound-list").click(function(){
+    $('#full-sound-list').slideUp();
+
+    if(!$('#full-sound-list').is(":visible")) {
+      $('#full-sound-list').slideDown();
+    }
+  });
+});
+
+function play(sound){
+  var name = $(sound).text().split(" ").join("");
+  var audio = document.getElementById(name);
+  audio.play();
+};
